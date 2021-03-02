9d3111f0fa01f384b65a281a99d33cf16c91c0eb
Ongyj888 <67620716+Ongyj888@users.noreply.github.com>
2021-03-02 17:18:02 +0800

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,5 +1,6 @@
 :a
-cd "C:\Program Files\Google\Chrome Beta\Application\
+cd "C:\Program Files\Google\Chrome Beta\Application\"
+cd "C:\Program Files\Google\Chrome\Application\"
 chrome "www.google.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth://totp/%random%?secret=JBSWY3DPEHPK3PXP&issuer=Example"
 Timeout /t 5 /nobreak
 goto a
