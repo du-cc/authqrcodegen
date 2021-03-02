09317062ca2f6f2e205b4ae33b396d3422b5277c
Ongyj888 <67620716+Ongyj888@users.noreply.github.com>
2021-03-02 17:16:28 +0800

    script.bat
    main script

---
+++ script.bat
@@ -0,0 +1,5 @@
+:a
+cd "C:\Program Files\Google\Chrome Beta\Application\
+chrome "www.google.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth://totp/%random%?secret=JBSWY3DPEHPK3PXP&issuer=Example"
+Timeout /t 5 /nobreak
+goto a
