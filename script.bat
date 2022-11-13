6bd8480f87bb21a8795493a0f8e7a3121eea2407
duccveloper <67620716+duccveloper@users.noreply.github.com>
2022-11-13 20:55:39 +0800

    Escaping special characters

--- script.bat
+++ script.bat
@@ -1,1 +1,1 @@
-start https://www.google.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth://totp/%random%?secret=JBSWY3DPEHPK3PXP&issuer=Example
+start https://www.google.com/chart?chs=200x200&chld=M^|0&cht=qr^&chl=otpauth://totp/%random%?secret=JBSWY3DPEHPK3PXP^&issuer=Example
